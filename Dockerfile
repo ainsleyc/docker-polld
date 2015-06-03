9f1b3fd663568d4d09ebfb58ca9113cc8e990bdb
Ainsley Chong <ainsley@factual.com>
2015-06-03 15:48:42 -0700

    Added autorunning shell script

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,8 @@
 FROM node:0.12.4
+
+RUN npm install -g polld
+
+RUN mkdir -p /home/app
+
+ADD ssh_config /root/.ssh/config
+ADD pull_app.sh /etc/my_init.d/001_git_pull_app
---
+++ pull_app.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "$GITHUB_SSH_KEY" > ~/.ssh/github_key && chmod 600 ~/.ssh/github_key
+
+GIT_BRANCH=${GIT_BRANCH:-"master"}
+
+cd /home/app
+
+if [ -z "$GIT_PATH" ]
+  then
+    git clone $GITHUB_REPO --branch $GIT_BRANCH --single-branch .
+  else
+    git clone -n $GITHUB_REPO --branch $GIT_BRANCH --single-branch .
+    git config core.sparseCheckout true
+    echo "$GIT_PATH" >> .git/info/sparse-checkout
+    git checkout $GIT_BRANCH
+    mv /home/app/$GIT_PATH/* /home/app
+fi
+
+npm install
+
+chown -R app /home/app
